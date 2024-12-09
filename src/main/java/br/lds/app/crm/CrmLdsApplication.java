0a20b0ca22f0b4b0ea443d69a9026730b4280049
Roger Pereira <116629830+RogerSouza1@users.noreply.github.com>
2024-12-09 15:52:27 -0300

    first commit

---
+++ src/main/java/br/lds/app/crm/CrmLdsApplication.java
@@ -0,0 +1,13 @@
+package br.lds.app.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmLdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmLdsApplication.class, args);
+	}
+
+}
